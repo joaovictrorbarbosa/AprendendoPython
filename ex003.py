95ea9a7376db3f28a0e4ab8efd2f5dc1dc55bc5f
Barbosa <joaovictorbarbosa2@gmail.com>
2024-11-19 22:23:18 -0300

    exercicio 3, indicando qual tipo primitivo é

---
+++ ex003.py
@@ -0,0 +1,6 @@
+n1 = input('digite algo para verificação: ')
+
+print('Abaixo mostrará se o valor colocado é numerico, alpha numerico e caracter')
+print(n1.isnumeric())
+print(n1.isalnum())
+print(n1.isalpha())

9cedbfb21e8e0148269a8a28f76f93fb83de618c
Barbosa <joaovictorbarbosa2@gmail.com>
2024-11-21 19:06:45 -0300

    exercicio 6

---
+++ ex006.py
@@ -0,0 +1,7 @@
+print( 'CALCULANDO MEDIA DE NOTAS')
+
+n1= int(input('Digite a primeira nota: '))
+n2= int(input('Digite a segunda nota: '))
+media= (n1+n2)/2
+
+print('A media deste aluno é {}'.format(media))

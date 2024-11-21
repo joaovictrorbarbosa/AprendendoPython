8bb28220202ab90dc331e164968792a42448e49d
Barbosa <joaovictorbarbosa2@gmail.com>
2024-11-21 19:39:22 -0300

    exercicio 11

---
+++ ex011.py
@@ -0,0 +1,12 @@
+print('QUANTO FICA O PRODUTO COM 5% DE DESCONTO?')
+
+p1=int(input('Digite qual o valor: '))
+d5= (p1*5)/100
+p5= p1 - d5
+
+print('O valor com 5% de desconto será de {}'.format(p5))
+
+
+
+
+

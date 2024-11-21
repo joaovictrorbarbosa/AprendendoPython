9ef51dcda5e566718f1732ed9bc07e434b64082c
Barbosa <joaovictorbarbosa2@gmail.com>
2024-11-21 19:16:03 -0300

    exercicio 8

---
+++ ex008.py
@@ -0,0 +1,17 @@
+from hmac import trans_36
+
+print('TABUADA')
+
+n1= int(input('digite um numero e descubra sua tabuada: '))
+t1= n1*1
+t2= n1*2
+t3= n1*3
+t4=n1*4
+t5=n1*5
+t6=n1*6
+t7=n1*7
+t8=n1*8
+t9=n1*9
+t10=n1*10
+
+print('A tabuada do numero escolhido é: \n 1: {} \n 2: {} \n 3: {} \n 4: {} \n 5: {} \n 6: {} \n 7: {} \n 8: {} \n 9: {} \n 10: {}'.format(t1,t2,t3,t4,t5,t6,t7,t8,t9,t10))

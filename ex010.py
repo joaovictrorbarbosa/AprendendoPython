34d10c1d41333e9fb9e08f4b087a6561bd7113e1
Barbosa <joaovictorbarbosa2@gmail.com>
2024-11-21 19:27:50 -0300

    exercicio 10

---
+++ ex010.py
@@ -0,0 +1,10 @@
+print('QUANTO DE TINTA É NECESSARIO PARA PINTAR A PAREDE?')
+
+pl= int(input('Digite a largura da parede: '))
+pa= int(input('Digite a altura da parede: '))
+a= pl*pa
+t= a/2
+
+print('A quantidade de tinta que deve ser usada é {} litros'.format(t))
+
+

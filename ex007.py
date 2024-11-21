eb0bbb9b2175a128b2079c5db2b84b4df8ea109f
Barbosa <joaovictorbarbosa2@gmail.com>
2024-11-21 19:10:01 -0300

    exercicio 7

---
+++ ex007.py
@@ -0,0 +1,6 @@
+print('CONVERTENDO MEDIDAS DE M PARA CM E MM')
+n1= int(input('Digite o valor em metros: '))
+cm= n1*100
+mm= n1*1000
+
+print('O valor de {} em centimetros é {} e em milimetros é {}'.format(n1,cm,mm))

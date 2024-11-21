db1e94e879c30e5830ae78019bb826f05239a6ec
Barbosa <joaovictorbarbosa2@gmail.com>
2024-11-21 17:59:38 -0300

    correção exercicio 3

--- ex003.py
+++ ex003.py
@@ -1,7 +1,8 @@
 n1 = input('digite algo para verificação: ')
 
-print('Abaixo mostrará qual o tipo primitivo do valor colocado, e se, ele é numerico, alpha numerico e caracter')
-print(type(n1))
-print(n1.isnumeric())
-print(n1.isalnum())
-print(n1.isalpha())
+
+print( 'O tipo primitivo deste valor é: ', type(n1))
+print( 'Só tem espaços? ', n1.isnumeric())
+print('É um numerico? ', n1.isalnum())
+print('É um alphanumerico? ', n1.isalpha())
+print('Tem caracteres? ', n1.isalpha() )

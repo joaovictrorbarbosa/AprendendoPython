e066317e2b257ab6bfd50f7e81c92dc5d56d7141
Barbosa <joaovictorbarbosa2@gmail.com>
2024-11-22 19:22:49 -0300

    exercicio 15

---
+++ ex0015.py
@@ -0,0 +1,8 @@
+import math
+
+a1= int(input('Digite o valor de seu angulo: '))
+sen= math.sin(a1)
+cos= math.cos(a1)
+tan = math.tan(a1)
+
+print('Os valor do seno do seu angulo é {:.2f} \n O valor do seu cosseno é {:.2f} \n O valor da sua tangente é {:.2f}'.format(sen,cos,tan))

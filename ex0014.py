15ba6e6641121cb1830e21f7ae0df11fc5790e51
Barbosa <joaovictorbarbosa2@gmail.com>
2024-11-22 19:18:15 -0300

    exercicio 14

---
+++ ex0014.py
@@ -0,0 +1,7 @@
+import math
+
+n1 = int(input('Digite o valor do cateto oposto: '))
+n2 = int(input('Digite o valor do cateto adjacente: '))
+hip = pow(n1,2) + pow(n2,2)
+
+print('O valor da hipotenusa é igual a {}'.format(hip))

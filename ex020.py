8e13daf6f318274f1d0213e3152d5af73809fec7
Barbosa <joaovictorbarbosa2@gmail.com>
2024-11-26 17:42:48 -0300

    exercicio 20 - analisando um numero

---
+++ ex020.py
@@ -0,0 +1,11 @@
+numero= int(input('Digite um numero: '))
+u = numero// 1 % 10
+d = numero // 10 % 10
+c = numero // 100% 10
+m = numero // 1000 % 10
+
+print('Analisando o numero: {}'.format(numero))
+print('Unidade: {}'.format(u))
+print('Dezena: {}'.format(d))
+print('Centena: {}'.format(c))
+print('milhar: {}'.format(m))
